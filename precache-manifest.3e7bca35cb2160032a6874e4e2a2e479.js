a091893b2f0efdbf38ba433f36a3565f92e291ff
Angela Han <angelahan.cc@gmail.com>
2023-01-24 12:00:59 +0900

    Updates

---
+++ precache-manifest.3e7bca35cb2160032a6874e4e2a2e479.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "61a0f7b530f06ace92df",
+    "url": "/react-tenzies/static/css/main.cd4a236d.chunk.css"
+  },
+  {
+    "revision": "61a0f7b530f06ace92df",
+    "url": "/react-tenzies/static/js/main.61a0f7b5.chunk.js"
+  },
+  {
+    "revision": "4587b35573b7dda0a77d",
+    "url": "/react-tenzies/static/js/1.4587b355.chunk.js"
+  },
+  {
+    "revision": "169a4e1afee697204427",
+    "url": "/react-tenzies/static/js/2.169a4e1a.chunk.js"
+  },
+  {
+    "revision": "d98bc50dcc0fca63b982",
+    "url": "/react-tenzies/static/js/runtime~main.d98bc50d.js"
+  },
+  {
+    "revision": "b7fd4fa07c43110107a890b7a98fc76b",
+    "url": "/react-tenzies/index.html"
+  }
+];
